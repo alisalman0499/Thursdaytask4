08dc98ea18d82ec465aaa72c02a9bc276b67fc41
axian04 <42780774+alisalman0499@users.noreply.github.com>
2025-09-25 21:06:00 +0200

    4f

--- Task4/GameMenu.java
+++ Task4/GameMenu.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 //4a
 public class GameMenu{
 	//4b
 	private ArrayList<String> actions = new ArrayList<String>();
 
+	//4c
 	public GameMenu(ArrayList<String> actions){
+		//4d
 		this.actions = actions;
 	}
 }
--- Task4/Main.java
+++ Task4/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+//4e
 public class Main{
 	public static void main(String[] args){
-		
+		//4f
+		ArrayList<String> actions = new ArrayList<String>();
+		actions.add("1) Start game");
+		actions.add("2) Resume game");
+		actions.add("3) Pause game");
+		actions.add("4) End game");
+
+		for (String action : actions){
+			System.out.println(action);
+		}
 	} 
 }
