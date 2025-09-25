cb8869831ec9165657d263d852aba17c1dd6ee5a
axian04 <42780774+alisalman0499@users.noreply.github.com>
2025-09-25 21:28:18 +0200

    5f

--- Task5/Main.java
+++ Task5/Main.java
@@ -19,9 +19,24 @@ public class Main{
 		
 		
 	} 
-	
+
 	//5e
 	public static void doAction(int action){
-
+		//5f
+		switch (action){
+			case 1:
+				System.out.println("Starting the game now");
+				break;
+			case 2:
+				System.out.println("Fetching previously saved game data");
+				break;
+			case 3:
+				System.out.println("Game paused");
+				break;
+			case 4:
+				System.out.println("Ending game");
+				break;
+			
 		}
+	}
 }
