badbe4166389170f370f4f1fac7a841a32886dae
axian04 <42780774+alisalman0499@users.noreply.github.com>
2025-09-25 18:49:22 +0200

    finished 1a to 1d

---
+++ Task1/Main.java
@@ -0,0 +1,6 @@
+//1a
+public class Main{
+	public static void main(String[] args){
+
+	}
+}
---
+++ Task1/Team.java
@@ -0,0 +1,11 @@
+//1b
+public class Team{
+	//1c
+	private String name;
+	//1d
+	private int rank;
+
+	public Team(){
+		
+	}
+}
