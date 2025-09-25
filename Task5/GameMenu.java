bc006e44bb0783ba614d66cee4c0e496ba8fc91e
axian04 <42780774+alisalman0499@users.noreply.github.com>
2025-09-25 21:17:22 +0200

    5a

---
+++ Task5/GameMenu.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+//4a
+public class GameMenu{
+	//4b
+	private ArrayList<String> actions = new ArrayList<String>();
+
+	//4c
+	public GameMenu(ArrayList<String> actions){
+		//4d
+		this.actions = actions;
+	}
+
+	//4h
+	public void displayMenu(){
+		for (String action : actions){
+			System.out.println(action);
+		}
+	}
+
+	//5a
+	public void promptUser(){
+		System.out.println("Type a number to choose an action: ");
+		displayMenu();
+	}
+}
---
+++ Task5/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+//4e
+public class Main{
+	public static void main(String[] args){
+		//4f
+		ArrayList<String> actions = new ArrayList<String>();
+		actions.add("1) Start game");
+		actions.add("2) Resume game");
+		actions.add("3) Pause game");
+		actions.add("4) End game");
+
+		//4g
+		GameMenu menu = new GameMenu(actions);
+		//4i
+		menu.promptUser();
+	} 
+}
